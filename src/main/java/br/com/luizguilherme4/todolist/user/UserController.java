040075fcd79744e8b9e9f35a9d39e36e8f8fcf37
Luiz Guilherme <101076506+luizguilherme4@users.noreply.github.com>
2023-10-12 13:28:34 -0400

    implementando auth

--- src/main/java/br/com/luizguilherme4/todolist/user/UserController.java
+++ src/main/java/br/com/luizguilherme4/todolist/user/UserController.java
@@ -1,5 +1,6 @@
 package br.com.luizguilherme4.todolist.user;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,6 +24,10 @@ public class UserController {
             System.out.println("Usuário já existe");
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
         }
+
+        var passwordHashred = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+
+        userModel.setPassword(passwordHashred);
 
         var userCreated = this.userRepository.save(userModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
